4d76b57f9d663f02ae0b06129542ac662bcd529e
PillageDev <pillageyt@gmail.com>
2024-10-27 21:03:31 -0400

    Added a cycle button

---
+++ core/src/main/java/mc/obliviate/inventory/CycleIcon.java
@@ -0,0 +1,38 @@
+package mc.obliviate.inventory;
+
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class CycleIcon<T> extends Icon {
+
+    private T value;
+    private final Map<T, ItemStack> toggleItems;
+    private final Consumer<T> onSwitch;
+
+    public CycleIcon(T initialValue, Map<T, ItemStack> toggleItems, Consumer<T> onSwitch) {
+        super(toggleItems.get(initialValue));
+
+        this.toggleItems = toggleItems;
+        this.onSwitch = onSwitch;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void nextValue() {
+        value = toggleItems.keySet().stream().skip((new ArrayList<>(toggleItems.keySet()).indexOf(value) + 1) % toggleItems.size()).findFirst().get();
+        onSwitch.accept(value);
+    }
+
+    @Override
+    public @NotNull Icon onClick(Consumer<InventoryClickEvent> clickAction) {
+        nextValue();
+        return this;
+    }
+}
